38134f5050e1de733ac678c13646b3be39df84c4
Exable1 <exable1412@gmail.com>
2018-02-26 22:08:50 +0300

    1.0
    First

---
+++ test.js
@@ -0,0 +1,58 @@
+var a,razm;
+function sem() {
+    textarea1.value+="7";
+}
+function rovno() {
+    textarea1.value = eval(textarea1.value);
+}
+
+function vosem() {
+    textarea1.value+="8";
+}
+function dev() {
+    textarea1.value+="9";
+}
+function pls() {
+    textarea1.value+="+";
+}
+function chet() {
+    textarea1.value+="4";
+}
+function pyat() {
+    textarea1.value+="5";
+}
+function shest() {
+    textarea1.value+="6";
+}
+function Odin() {
+    textarea1.value +="1";
+}
+function Dva() {
+    textarea1.value +="2";
+}
+function Tri() {
+    textarea1.value+="3";
+}
+function nol() {
+    textarea1.value+="0";
+}
+function Clear() {
+    textarea1.value="";
+}
+function min() {
+    textarea1.value+="-";
+}
+function ymnozh() {
+    textarea1.value+="*";
+}
+function Delen() {
+    textarea1.value+="/";
+}
+function scob1() {
+    textarea1.value+="(";
+}
+function scob2() {
+    textarea1.value+=")";
+}
+
+
