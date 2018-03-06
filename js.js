be3e1ac59f2c9f1593716410a9d4d9c0956c66cb
Exable1 <exable1412@gmail.com>
2018-03-06 10:26:09 +0300

    3.3

---
+++ js.js
@@ -0,0 +1,72 @@
+function sortir1() {
+    document.getElementById( 'item1' ).style.display = 'block';
+    document.getElementById( 'item2' ).style.display = 'block';
+    document.getElementById( 'item3' ).style.display = 'block';
+    document.getElementById( 'item4' ).style.display = 'block';
+    document.getElementById( 'item5' ).style.display = 'none';
+    document.getElementById( 'item6' ).style.display = 'none';
+    document.getElementById( 'item7' ).style.display = 'none';
+    document.getElementById( 'item8' ).style.display = 'none';
+    document.getElementById( 'item9' ).style.display = 'none';
+    document.getElementById( 'item10' ).style.display = 'none';
+    document.getElementById( 'item11' ).style.display = 'none';
+    document.getElementById( 'item12' ).style.display = 'none';
+    document.getElementById( 'item13' ).style.display = 'none';
+    document.getElementById( 'item14' ).style.display = 'none';
+    document.getElementById( 'item15' ).style.display = 'none';
+    document.getElementById( 'item16' ).style.display = 'none';
+}
+function sortir2() {
+    document.getElementById( 'item1' ).style.display = 'none';
+    document.getElementById( 'item2' ).style.display = 'none';
+    document.getElementById( 'item3' ).style.display = 'none';
+    document.getElementById( 'item4' ).style.display = 'none';
+    document.getElementById( 'item5' ).style.display = 'block';
+    document.getElementById( 'item6' ).style.display = 'block';
+    document.getElementById( 'item7' ).style.display = 'block';
+    document.getElementById( 'item8' ).style.display = 'block';
+    document.getElementById( 'item9' ).style.display = 'none';
+    document.getElementById( 'item10' ).style.display = 'none';
+    document.getElementById( 'item11' ).style.display = 'none';
+    document.getElementById( 'item12' ).style.display = 'none';
+    document.getElementById( 'item13' ).style.display = 'none';
+    document.getElementById( 'item14' ).style.display = 'none';
+    document.getElementById( 'item15' ).style.display = 'none';
+    document.getElementById( 'item16' ).style.display = 'none';
+}
+function sortir3() {
+    document.getElementById( 'item1' ).style.display = 'none';
+    document.getElementById( 'item2' ).style.display = 'none';
+    document.getElementById( 'item3' ).style.display = 'none';
+    document.getElementById( 'item4' ).style.display = 'none';
+    document.getElementById( 'item5' ).style.display = 'none';
+    document.getElementById( 'item6' ).style.display = 'none';
+    document.getElementById( 'item7' ).style.display = 'none';
+    document.getElementById( 'item8' ).style.display = 'none';
+    document.getElementById( 'item9' ).style.display = 'block';
+    document.getElementById( 'item10' ).style.display = 'block';
+    document.getElementById( 'item11' ).style.display = 'block';
+    document.getElementById( 'item12' ).style.display = 'block';
+    document.getElementById( 'item13' ).style.display = 'none';
+    document.getElementById( 'item14' ).style.display = 'none';
+    document.getElementById( 'item15' ).style.display = 'none';
+    document.getElementById( 'item16' ).style.display = 'none';
+}
+function sortir4() {
+    document.getElementById( 'item1' ).style.display = 'none';
+    document.getElementById( 'item2' ).style.display = 'none';
+    document.getElementById( 'item3' ).style.display = 'none';
+    document.getElementById( 'item4' ).style.display = 'none';
+    document.getElementById( 'item5' ).style.display = 'none';
+    document.getElementById( 'item6' ).style.display = 'none';
+    document.getElementById( 'item7' ).style.display = 'none';
+    document.getElementById( 'item8' ).style.display = 'none';
+    document.getElementById( 'item9' ).style.display = 'none';
+    document.getElementById( 'item10' ).style.display = 'none';
+    document.getElementById( 'item11' ).style.display = 'none';
+    document.getElementById( 'item12' ).style.display = 'none';
+    document.getElementById( 'item13' ).style.display = 'block';
+    document.getElementById( 'item14' ).style.display = 'block';
+    document.getElementById( 'item15' ).style.display = 'block';
+    document.getElementById( 'item16' ).style.display = 'block';
+}
