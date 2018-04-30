91b79a81f357267a2b307a3e1aba1f9ede1a1381
exable1 <33894283+exabletop1@users.noreply.github.com>
2018-04-30 14:10:06 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,4 @@
 function sortir1() {
-    document.getElementById( 'item1' ).style.display = 'block';
-    document.getElementById( 'item2' ).style.display = 'block';
-    document.getElementById( 'item3' ).style.display = 'block';
-    document.getElementById( 'item4' ).style.display = 'block';
     document.getElementById( 'item5' ).style.display = 'none';
     document.getElementById( 'item6' ).style.display = 'none';
     document.getElementById( 'item7' ).style.display = 'none';
@@ -21,10 +17,6 @@ function sortir2() {
     document.getElementById( 'item2' ).style.display = 'none';
     document.getElementById( 'item3' ).style.display = 'none';
     document.getElementById( 'item4' ).style.display = 'none';
-    document.getElementById( 'item5' ).style.display = 'block';
-    document.getElementById( 'item6' ).style.display = 'block';
-    document.getElementById( 'item7' ).style.display = 'block';
-    document.getElementById( 'item8' ).style.display = 'block';
     document.getElementById( 'item9' ).style.display = 'none';
     document.getElementById( 'item10' ).style.display = 'none';
     document.getElementById( 'item11' ).style.display = 'none';
@@ -43,10 +35,6 @@ function sortir3() {
     document.getElementById( 'item6' ).style.display = 'none';
     document.getElementById( 'item7' ).style.display = 'none';
     document.getElementById( 'item8' ).style.display = 'none';
-    document.getElementById( 'item9' ).style.display = 'block';
-    document.getElementById( 'item10' ).style.display = 'block';
-    document.getElementById( 'item11' ).style.display = 'block';
-    document.getElementById( 'item12' ).style.display = 'block';
     document.getElementById( 'item13' ).style.display = 'none';
     document.getElementById( 'item14' ).style.display = 'none';
     document.getElementById( 'item15' ).style.display = 'none';
@@ -65,16 +53,34 @@ function sortir4() {
     document.getElementById( 'item10' ).style.display = 'none';
     document.getElementById( 'item11' ).style.display = 'none';
     document.getElementById( 'item12' ).style.display = 'none';
-    document.getElementById( 'item13' ).style.display = 'block';
-    document.getElementById( 'item14' ).style.display = 'block';
-    document.getElementById( 'item15' ).style.display = 'block';
-    document.getElementById( 'item16' ).style.display = 'block';
+
 }
-$(document).ready(function(){
-	$("navarNav").on("click","a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1000);
-	});
+    $(document).ready(function(){
+
+    $('.ruki').click(function(){
+        for(i=1;i<=4;i++){
+            $('#item'+i).fadeIn(700);
+        }
+    })
+    $('.smaz').click(function(){
+        for(i=5;i<=8;i++){
+            $('#item'+i).fadeIn(700);
+        }
+    })
+    $('.masl').click(function(){
+        for(i=9;i<=12;i++){
+            $('#item'+i).fadeIn(700);
+        }
+    })
+    $('.tehzhid').click(function(){
+        for(i=13;i<=16;i++){
+            console.log(i)
+            $('#item'+i).fadeIn(700);
+        }
+    })
+    $('.reset').click(function(){
+        for(i=1;i<=16;i++){
+            $('#item'+i).fadeIn(700);
+        }
+    })
 });
